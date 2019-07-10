541d363432647933e046ebc79440cb138fc62fbd
Nina B <balan.nyna@gmail.com>
2019-07-10 19:41:23 +0300

    odul-3

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,74 @@
-import React, { Component } from 'react';
-import moment from "moment" ;
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom" ;
 
-import PortfolioContainer from "./portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail.js";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+  }
+   
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
   render() {
     return (
-      <div className='app'>
-        <h1>NinaB Portfolio</h1>
-        <PortfolioContainer />
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+      <div className="container">
+        <Router>
+          <div>
+          <NavigationContainer />
+
+          <Switch>
+            <Route exact path="/"  component={Home} />
+
+            <Route 
+              path="/auth"
+              render={props => (
+                <Auth
+                  {...props}
+                  handleSuccessfulLogin={this.handleSuccessfulLogin}
+                  handleUnSuccessfulLogin={this.handleSuccessfulLogin}
+                />
+              )}
+            />
+
+            <Route path="/about-me"  component={About} />
+            <Route path="/contact"  component={Contact} />
+            <Route path="/blog"  component={Blog} />
+            <Route path="/portfolio/:slug"component={PortfolioDetail} />
+            <Route
+              exact
+              path="/portfolio/:slug"
+              component={PortfolioDetail} 
+            />
+            <Route component={NoMatch} />
+          </Switch>
+          </div>
+        </Router>
+
       </div>
     );
   }
