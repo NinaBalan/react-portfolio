6a43901b4ffdef9c944c48ae9cac10f199551249
Nina B <balan.nyna@gmail.com>
2019-07-12 22:52:44 +0300

    modul4

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,8 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
-import PortfolioDetail from "./portfolio/portfolio-detail.js";
+import PortfolioManager from "./pages/portfolio-manager";
+import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
@@ -21,6 +22,7 @@ export default class App extends Component {
 
     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
     this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
    
   handleSuccessfulLogin() {
@@ -30,6 +32,11 @@ export default class App extends Component {
   }
 
   handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+  handleSuccessfulLogout() {
     this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
     });
@@ -69,15 +76,25 @@ export default class App extends Component {
     componentDidMount() {
     this.checkLoginStatus();
   }
+  authorizedPages() {
+    return [
+      <Route 
+        key="portfolio-manager" 
+        path="/portfolio-manager"  
+        component={PortfolioManager} 
+      />
+    ];
+  }
 
   render() {
     return (
       <div className="container">
         <Router>
           <div>
-          <NavigationContainer />
-
-          <h2>{this.state.loggedInStatus}</h2>
+          <NavigationContainer
+            loggedInStatus={this.state.loggedInStatus}
+            handleSuccessfulLogout={this.handleSuccessfulLogout}
+          />
 
           <Switch>
             <Route exact path="/"  component={Home} />
@@ -95,8 +112,10 @@ export default class App extends Component {
 
             <Route path="/about-me"  component={About} />
             <Route path="/contact"  component={Contact} />
-            <Route path="/blog"  component={Blog} />
-            <Route path="/portfolio/:slug"component={PortfolioDetail} />
+            <Route path="/blog" component={Blog} />
+            {this.state.loggedInStatus === "LOGGED_IN" ? (
+              this.authorizedPages()
+            ) : null}
             <Route
               exact
               path="/portfolio/:slug"
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,17 +1,34 @@
 import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
-import { prependOnceListener } from "cluster";
 
 const NavigationComponent = (props) => {
     const dynamicLink = (route, linkText) => {
         return (
-        <div className="nav-link-wrapper">
-            <NavLink to="blog" activeClassName="nav-link-active">
-                Blog
-            </NavLink>
-        </div>
+            <div className="nav-link-wrapper">
+                <NavLink to={route} activeClassName="nav-link-active">
+                   {linkText}
+                </NavLink>
+            </div>
         );
-    }; 
+    };
+    
+    const handleSingnOut =() => {
+        axios
+        .delete("https://api.devcamp.space/logout",{ withCredentials: true})
+        .then(response => {
+            if(response.status === 200) {
+                props.history.push ("/");
+                props.handleSuccessfulLogout();
+            }
+            return response.data;
+        })
+        .catch(error => {
+            console.log("Error signing out", error);
+        });
+    };
+
 
     return (
             <div className="nav-wrapper">
@@ -22,25 +39,40 @@ const NavigationComponent = (props) => {
                         </NavLink>
                     </div>
                     <div className="nav-link-wrapper">
-                        <NavLink to="about-me" activeClassName="nav-link-active">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">
                             About
                         </NavLink>
                     </div>
                     <div className="nav-link-wrapper">
-                        <NavLink to="contact" activeClassName="nav-link-active">
+                        <NavLink to="/contact" activeClassName="nav-link-active">
                             Contact
                         </NavLink>
                     </div>
 
-                    {true ? "do this" : "do something else"}
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/portfolio-manager" activeClassName="nav-link-active">
+                            Portfolio Manager
+                        </NavLink>
+                    </div>
 
                     {props.loggedInStatus === "LOGED_IN" ? ( 
-                        dynamicLink("/blog", "Blog") 
+                        dynamicLink("/portfolio-manager", "Portfolio Manager") 
                     ): null}
                 </div>
                 
-                <div className="right-side">Nina Balan</div>
+                <div className="right-side">
+                    Nina Balan
+                    {props.loggedInStatus === "LOGGED_IN" ? (
+                        <a onClick={handleSingnOut}>Sign Out</a>
+                    ) : null}
+                </div>
             </div>
         );
     }
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
